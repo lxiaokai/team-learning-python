160f5cba1c157d1ae7bc8b35006a1c41a366d4e5
Liangk <liangk@Liangk-MacBook-Pro.local>
2018-10-15 16:36:15 +0800

    no message

--- 取个队名真难-K/day01/helloworld.py
+++ 取个队名真难-K/day01/helloworld.py
@@ -6,7 +6,17 @@
 # @File    : helloworld.py
 # @Software: PyCharm
 
+import datetime
+import time
 
 # 程序主入口
 if __name__ == '__main__':
+
     print("Hello World!")
+
+    i = 0
+    while i < 4:
+        nowDate = time.strftime("%Y年%m月%d日 %I:%M:%S")
+        print("Hello World! %s" % nowDate)
+        i += 1
+        time.sleep(1)
