036ec4f918b6f45e6807e569d014ba9792b66831
lhf-lhf <1057705752@qq.com>
2018-10-15 15:42:43 +0800

    test
    建立文件夹，提交day01

---
+++ 取个队名真难-F/day01/day1.py
@@ -0,0 +1,9 @@
+
+__author__ = 'LHF'
+if __name__ =="__main__":
+    # i=0
+    # while i<100:
+    #     i+=1
+    #     print("hello world")
+    for i in range(0,100):
+        print("hello world"+str(i))
