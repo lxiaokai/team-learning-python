de701b6dc28a9d2a1051810b840f94d94ceeb35d
cuizhongyi <cuizhongyi@zdlhcar.com>
2018-10-16 11:39:17 +0800

    '一次性赋值多个变量打印'

---
+++ 取个队名真难-C/day02/ide.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 程序主入口
+if __name__ == '__main__':
+    company, date, *others = ['juejin', '20181016', 'morning', 'Tues']
+    print(company, date, others)
