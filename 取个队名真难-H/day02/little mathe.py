c0eb311f9d2f714d0cdbb096d48d6a2ac131e12e
sallyhuang302 <812167344@qq.com>
2018-10-16 14:56:57 +0800

    更新作业

---
+++ 取个队名真难-H/day02/little mathe.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+flag = 1
+while True:
+    val = input("请输入一个方式；1加，2减，3乘法，4除:")
+    num1 = input("请输入一个数字：")
+    num2 = input("请输入一个数字")
+    if val == "1":
+        sum = float(num1) + float(num2)
+        print('数字 {0} 和 {1} 相加结果为： {2}'.format(num1, num2, sum))
+    elif val == "2":
+        subctr = float(num1) - float(num2)
+        print('数字 {0} 和 {1} 相减结果为： {2}'.format(num1, num2, subctr))
+    elif val == "3":
+        mutil = float(num1) * float(num2)
+        print('数字 {0} 和 {1} 相乘结果为： {2}'.format(num1, num2, mutil))
+    elif val == "4":
+        divi = float(num1) / float(num2)
+        print('数字 {0} 和 {1} 相乘结果为： {2}'.format(num1, num2, divi))
+    elif val == "q":
+        flag = 0
+        break
+    else:
+        print("非法输入,error")
