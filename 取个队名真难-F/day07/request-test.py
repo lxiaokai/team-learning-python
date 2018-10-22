c40ec4fec292924129e8da58d2053cb10749b49e
lhf-lhf <1057705752@qq.com>
2018-10-22 15:07:08 +0800

    request新增下载图片内容

--- 取个队名真难-F/day07/request-test.py
+++ 取个队名真难-F/day07/request-test.py
@@ -1,6 +1,8 @@
-# BY LHF
-import json
+# @time 2018/10/22 10:11
+# @Author lhf
+
 import os
+from urllib import parse
 
 import requests
 
@@ -12,29 +14,57 @@ def getRequest(n):
     return requestResult
 
 
+def makeDir(s):
+    if not os.path.exists(s):
+        os.makedirs(s)
+
+
+def download_img(url, name):
+    # 图片名称
+    url_data = parse.urlparse(url=url)
+    # 图片请求参数
+    url_param = parse.parse_qs(url_data.query)
+    try:
+        # 图片扩展名
+        img_ext = url_param['f'][0]  # 有可能为空
+    except:
+        print("error:", url_param);
+        img_ext = '.jpg'
+    # 图片路径
+    url_path = url_data.path
+    # 图片标识ID
+    img_id = os.path.split(url_path)[1]
+    # 图片名称
+    if (name == ''):
+        img_name = img_id + '.' + img_ext
+    else:
+        img_name = name
+    img_path = os.path.join("img", img_name)
+    with open(img_path, 'wb') as o:
+        pic = requests.get(url)
+        o.write(pic.content)
+
+
 if __name__ == '__main__':
     requestResult = getRequest(URL)
     print('结果=', requestResult.status_code)
     dict1 = requestResult.json()
     list = dict1['d']['list']
     # print(dict)
-    # print(list)
     # text=open('text.txt','a',encoding='utf-8')  拼接的模式写入
-    if os.path.exists('user') == False:
-        os.makedirs('user')
-    if os.path.exists('img') == False:
-        os.makedirs('img')
+    makeDir('user')
+    makeDir('img')
     text = open('user/user.txt', 'w', encoding='utf-8')  # 替换的模式写入
-    img = open('img/img.txt', 'w', encoding='utf-8')  # 替换的模式写入
+    imgText = open('img/img.txt', 'w', encoding='utf-8')  # 替换的模式写入
     text.writelines('获取的到user:' + "\n")
-    img.write('图片链接：' + '\n')
+    # imgText.write('图片链接：' + '\n')
     for i in list:
         text.write(i['user']['username'] + '\n')  # 换行
-        # text.writelines(i['user']['username'])
-        img.write(str(i['pictures']) + '\n')
-
+        for j in i['pictures']:
+            imgText.writelines(j + "\n")
+            download_img(j, '')
     text.close()
-    img.close()
+    imgText.close()
     with open('user/user.txt', 'r', encoding='utf-8') as f:
         print(f.read())
     with open('img/img.txt', 'r', encoding='utf-8') as k:
