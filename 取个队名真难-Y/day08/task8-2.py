b75639c600e53a8268c31b551c70b81c660a88b6
yyh <yuhong54@163.com>
2018-10-24 22:22:20 +0800

    learn IO

---
+++ 取个队名真难-Y/day08/task8-2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# 判断是不是素数
+def log(func):
+    def wrapper(*args,**kw):
+        print('call %s():' % func.__name__)
+        return func(*args,**kw)
+    return wrapper
+
+@log
+def isPrime(nums,dictonary):
+    for num in nums:
+        if num > 1:
+            for i in range(2,num):
+                if (num % i)== 0:
+                    break
+            else:
+                dictonary[str(num)] = '素数'
+    return dictonary
+    
+if __name__ =="__main__":
+    dictionary = {}
+    nums = [1,2,3,4,5,6,7,8,9,11,13]
+
+    print(isPrime(nums,dictionary))
+
