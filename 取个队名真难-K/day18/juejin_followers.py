5fb16c123974b7156f0e166d3ecdde9bb010f60f
Liangk <liangk@Liangk-MacBook-Pro.local>
2018-11-06 15:16:07 +0800

    修改分页爬取情况
    因为之前没有关注很多人,所以不清楚这个分页的情况,现在补充上去分页的,关键参数是"before",获取返回来的"createdAtString"替换即可

--- 取个队名真难-K/day18/juejin_followers.py
+++ 取个队名真难-K/day18/juejin_followers.py
@@ -10,11 +10,16 @@ import requests
 import json
 
 
-def get_follower_data(url):
+def get_follower_data(url, data):
     header = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
     }
-    request = requests.get(url=url, headers=header)
+    params = {}
+    params.update({'uid': '59b0de6e5188250f4850ea06'})
+    params.update({'currentUid': '59b0de6e5188250f4850ea06'})
+    params.update({'src': 'web'})
+    params.update({'before': data})
+    request = requests.get(url=url, params=params, headers=header)
     if request.status_code == 200:
         return request.json()['d']
 
@@ -29,10 +34,17 @@ def save_by_json(data):
 
 # 程序主入口
 if __name__ == '__main__':
-    request_url = 'https://follow-api-ms.juejin.im/v1/getUserFollowerList?uid=5bbf1f7fe51d450e8108eec4&currentUid=5bbf1f7fe51d450e8108eec4&src=web'
-    datalist = get_follower_data(request_url)
+    request_url = 'https://follow-api-ms.juejin.im/v1/getUserFollowerList'
+    create_date = ''
     follower_list = []
-    for i in datalist:
-        print('关注>>>', 'ID:', i['follower']['objectId'], '昵称:', i['follower']['username'])
-    print('一共关注了%s人' % len(datalist))
+    while True:
+        datalist = get_follower_data(request_url, create_date)
+        if not datalist:
+            break
+        for i in datalist:
+            print('关注>>>', 'ID:', i['follower']['objectId'], '昵称:', i['follower']['username'])
+            create_date = i['createdAtString']
+            follower_list.append(i['follower'])
+
+    print('一共关注了%s人' % len(follower_list))
     save_by_json(datalist)
