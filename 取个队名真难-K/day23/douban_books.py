85252128df681c472acda8ae2467c4426612f9b6
Liangk <liangk@Liangk-MacBook-Pro.local>
2018-11-22 17:47:11 +0800

    代理IP问题

--- 取个队名真难-K/day23/douban_books.py
+++ 取个队名真难-K/day23/douban_books.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# @Time    : 2018/11/16 3:13 PM
+# @Time    : 2018/11/22
 # @Author  : liangk
 # @Site    :
 # @File    : get_douban_books.py
@@ -9,58 +9,68 @@
 from bs4 import BeautifulSoup
 import datetime
 import requests
-from change_ip import Change_IP
+import json
 import random
 
+ip_random = -1
 article_tag_list = []
 article_type_list = []
-proxy_list = []
-result = []
 
 
-def get_html(url, ip):
-
+def get_html(url):
     header = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
     }
-    proxies = {
-        "https": ip,
-    }
+    global ip_random
+    ip_rand, proxies = get_proxie(ip_random)
+    print(proxies)
     try:
-        request = requests.get(url=url, headers=header, proxies=proxies)
-        request.encoding = 'gbk'
+        request = requests.get(url=url, headers=header, proxies=proxies, timeout=3)
+    except:
+        request_status = 500
+    else:
+        request_status = request.status_code
+    print(request_status)
+    while request_status != 200:
+        ip_random = -1
+        ip_rand, proxies = get_proxie(ip_random)
         print(proxies)
-        if request.status_code == 200:
-            html = request.content
-            return html, True
-    except Exception as e:
-        print(e)
-        return e, False
+        try:
+            request = requests.get(url=url, headers=header, proxies=proxies, timeout=3)
+        except:
+            request_status = 500
+        else:
+            request_status = request.status_code
+        print(request_status)
+    ip_random = ip_rand
+    request.encoding = 'gbk'
+    html = request.content
+    print(html)
+    return html
+
+
+def get_proxie(random_number):
+    with open('ip.txt', 'r') as file:
+        ip_list = json.load(file)
+        if random_number == -1:
+            random_number = random.randint(0, len(ip_list) - 1)
+        ip_info = ip_list[random_number]
+        ip_url_next = '://' + ip_info['address'] + ':' + ip_info['port']
+        proxies = {'http': 'http' + ip_url_next, 'https': 'https' + ip_url_next}
+        return random_number, proxies
 
 
 # 程序主入口
 if __name__ == '__main__':
-    # Change_IP().main()
 
-    fp = open('ip_https.txt', 'r')
-    ips = fp.readlines()
-    proxy_list = []
-    for p in ips:
-        ip = p.strip('\n')
-        proxy = 'https://' + ip
-        proxy_list.append(proxy)
+
 
     """只是爬取了书籍的第一页,按照评价排序"""
     start_time = datetime.datetime.now()
+
     url = 'https://book.douban.com/tag/?view=type&icn=index-sorttags-all'
     base_url = 'https://book.douban.com/tag/'
-
-    result_status = False
-    html = ''
-    ip = random.choice(proxy_list)
-    if not result_status:
-        html, result_status = get_html(url, ip)
-
+    html = get_html(url)
     soup = BeautifulSoup(html, 'lxml')
     article_tag_list = soup.find_all(class_='tag-content-wrapper')
     tagCol_list = soup.find_all(class_='tagCol')
@@ -78,22 +88,22 @@ if __name__ == '__main__':
             title = '==============' + sub1 + '=============='
             print(title)
             print(base_url + sub1 + '?start=0' + '&type=S')
-            for start in range(0, 1):
+            with open('book.text', 'a', encoding='utf-8') as f:
+                f.write('\n' + title + '\n')
+                f.write(url + '\n')
+            for start in range(0, 2):
                 # (start * 20) 分页是0 20  40 这样的
                 # type=S是按评价排序
                 url = base_url + sub1 + '?start=%s' % (start * 20) + '&type=S'
-                with open('book.text', 'a') as f:
-                    f.write('\n' + title + '\n')
-                    f.write(url + '\n')
-                html = get_html(url, ip)
+                html = get_html(url)
                 soup = BeautifulSoup(html, 'lxml')
                 li = soup.find_all(class_='subject-item')
                 for div in li:
                     info = div.find(class_='info').find('a')
                     img = div.find(class_='pic').find('img')
-                    content = '书名:<%s>' % info['title'] + '  书名图片:' + img['src'] + '\n'
+                    content = '书名:<%s>' % info['title'] + '  书本图片:' + img['src'] + '\n'
                     print(content)
-                    with open('book.text', 'a') as f:
+                    with open('book.text', 'a', encoding='utf-8') as f:
                         f.write(content)
 
     end_time = datetime.datetime.now()
